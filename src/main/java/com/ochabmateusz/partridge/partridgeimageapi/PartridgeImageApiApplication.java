26e5657d12179d67ffd6fd10133a44e7a3cf1ef7
Mateusz Ochab <ochab.mat@gmail.com>
2019-03-20 14:12:56 +0100

    initial commit

---
+++ src/main/java/com/ochabmateusz/partridge/partridgeimageapi/PartridgeImageApiApplication.java
@@ -0,0 +1,13 @@
+package com.ochabmateusz.partridge.partridgeimageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartridgeImageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartridgeImageApiApplication.class, args);
+	}
+
+}
